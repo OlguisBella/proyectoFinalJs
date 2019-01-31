2a903a251f6156c1e76edc7008157865500422b6
OlguisBella <olgseb@gmail.com>
2019-01-31 00:09:47 -0500

    modificación de game.js

--- server/routes/game.js
+++ server/routes/game.js
@@ -17,10 +17,8 @@ router.get('/admin', function(req, res, next) {
   res.render('admin', {page:'Administrador', menuId:'admin'});
 });
 
-<<<<<<< HEAD
+
 //Para las imagenes
 
 
-=======
->>>>>>> 88b9090a2d553778c81f64d39cb6bf8ce8f541f1
 module.exports = router;
